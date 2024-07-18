5ddca5191faf5751fd93777415f573d57da45cb0
Jruiz1980 <rui18007@byui.edu>
2024-07-17 20:36:29 -0500

    add header

---
+++ spices/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    
+  ],
+  imports: [
+    AppComponent,
+    HeaderComponent,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
